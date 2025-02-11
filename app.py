451f6d08185d5b32aba76d752d31148c72fee408
adfinem <zenlaws02@gmail.com>
2025-02-11 21:43:28 +0800

    feat: additional code

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ model = YOLO("yolov8n.pt")
 # Train the model
 train_results = model.train(
     data="config.yaml",  # path to dataset YAML
-    epochs=5,  # number of training epochs
+    epochs=50,  # number of training epochs
     device="cpu",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
 )
 
@@ -14,9 +14,12 @@ train_results = model.train(
 metrics = model.val()
 
 # Perform object detection on an image
-results = model("images/sample.jpg")
+results = model("images/sample.jpg", conf=0.25)
 results[0].show()
-print(results[0].boxes)
+if results[0].boxes is not None and len(results[0].boxes) > 0:
+    print("Bounding boxes detected:", results[0].boxes)
+else:
+    print("No bounding boxes detected.")
 
 # Export the model to ONNX format
 path = model.export(format="onnx")  # return path to exported model
