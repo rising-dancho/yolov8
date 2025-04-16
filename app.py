fba651c24bc9d55ee85f78ef5c5acfb272157de7
rising-dancho <zenlaws02@gmail.com>
2025-04-16 12:16:12 +0800

    feat: added conversion script from .pt to .ptl

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ import glob
 # Load a model and train it
 model = YOLO("yolov8n.pt")
 model.train(
-    data="config.yaml",
-    epochs=1,
-    device="cpu",
-) # transfer learning happens automatically during this step
+    data="config.yaml", epochs=5, device="cpu", imgsz=640
+)  # transfer learning happens automatically during this step
 
 # Automatically find the latest trained model
 best_model_path = sorted(glob.glob("runs/detect/train*/weights/best.pt"))[-1]
@@ -22,5 +20,5 @@ results = trained_model("images/sacks.jpg", conf=0.25)
 results[0].show()
 
 # ✅ Export trained modelo
-path = trained_model.export(format="torchscript") 
+path = trained_model.export(format="torchscript")
 print("Model exported to:", path)
